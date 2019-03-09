db9ee814029ae5b80cc7a42ba203892e0cffe837
Jason Weir <jason.g.weir@gmail.com>
2019-03-09 11:13:58 -0700

    Make PWA

---
+++ sw.js
@@ -0,0 +1,1 @@
+importScripts("/cache-polyfill.js");self.addEventListener("install",function(e){e.waitUntil(caches.open("gidgidonihah").then(function(cache){return cache.addAll(["/","/index.html"])}))});self.addEventListener("fetch",function(event){event.respondWith(caches.match(event.request).then(function(response){return response||fetch(event.request)}))});
